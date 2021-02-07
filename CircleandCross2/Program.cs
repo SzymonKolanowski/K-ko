6b880c6b0cd8c669a90779b4ad39a06a5650af78
Szymon Kolanowski <szymonkolanowski@op.pl>
2021-02-07 16:21:35 +0100

    Dodano program

---
+++ CircleandCross2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace CircleandCross2
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("CircleandCross");
+
+			//string[,] array = new string [3, 3];
+
+			string[,] array = new string[3,3] { {"x","o","o" }, {"x","x","o"}, {"x","o","o" } };
+
+
+
+
+
+			for (int i = 0; i < 3; i++)
+			{
+
+				Console.WriteLine(array);
+
+			}
+
+			//for (int i = 0; i < array.Length; i++)
+			//{
+			//	string arr = array[i];
+			//}
+
+
+		}
+	}
+}
