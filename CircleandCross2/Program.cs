475c2b687b68d6c1b712c89c09cd8772bbaa34b5
Szymon Kolanowski <szymonkolanowski@op.pl>
2021-02-09 16:37:14 +0100

    Dzisiejsze poprawki - w lini 38 zakomentowałem 2 pytania

--- CircleandCross2/Program.cs
+++ CircleandCross2/Program.cs
@@ -4,21 +4,20 @@ namespace CircleandCross2
 {
 	class Program
 	{
+		static string[] Savearr(string[] array)
+		{
+			for (int i = 1; i < array.Length; i++)
+			{
+				array[i] = array[i] + array[i - 1];
+			}
+			return array;
+		}
 		static void Main(string[] args)
 		{
 			Console.WriteLine("CircleandCross");
-
-			//string[,] array = new string [3, 3];
-
+			
 			string[,] array = new string[3,3] { {"[ ]","[ ]","[ ]" }, {"[ ]","[ ]","[ ]"}, {"[ ]","[ ]","[ ]" } };
 			
-
-
-
-
-
-
-
 			for (int i = 0; i <array.GetLength(0); i++)
 			{
 				for (int j = 0; j < array.GetLength(1); j++)
@@ -27,8 +26,6 @@ namespace CircleandCross2
 					Console.Write(arr);
 				}
 				Console.WriteLine();
-				
-
 			}
 
 			Console.WriteLine("podaj pozycje 'o' w lini 'i'");
@@ -37,6 +34,8 @@ namespace CircleandCross2
 			Console.WriteLine(" podaj pozycję 'o' w lini 'j' ");
 			string tekst2 = Console.ReadLine();
 			int pozycja1j = int.Parse(tekst2);
+
+			//string[] Cumulativearr = Savearr(array); - nie wiem jak zapisać funcję do tablicy 2-wymiarowej i w którym miejscu dać tą funkcję
 
 			for (int i = 0; i < array.GetLength(0); i++)
 			{
@@ -47,21 +46,21 @@ namespace CircleandCross2
 					//{
 					//	Console.Write("o");
 					//}
-					 if (j == pozycja1j)
+					 if (j == pozycja1j )
 					{
 						Console.Write("o");
 					}
 					else
 					{
-						string arr1 = array[pozycja1i, pozycja1j];
+						string arr1 = array[pozycja1i,pozycja1j ];
 						Console.Write(arr1);
 					}
 				}
 
-				//if (i == pozycja1i)
-				//{
-				//	Console.Write("o");
-				//}
+				if (i == pozycja1i)
+				{
+					Console.Write("o");
+				}
 
 				//else
 				//{
@@ -74,6 +73,29 @@ namespace CircleandCross2
 				Console.WriteLine();
 			
 				
+			}
+
+			Console.WriteLine("podaj pozycję znaku 'x' w lini i");
+			string tekst3 = Console.ReadLine();
+			int pozycja2i = int.Parse(tekst3);
+			Console.WriteLine("podaj pozycję znaku 'x' w lini j");
+			string tekst4 = Console.ReadLine();
+			int pozycja2j = int.Parse(tekst4);
+
+
+			for (int i = 0; i < array.GetLength(0); i++)
+			{
+				for (int j = 0; j < array.GetLength(1); j++)
+				{
+					if (j == pozycja2j)
+					{
+						Console.WriteLine("x");
+					}
+					else
+					{
+						string arr2 = array[pozycja2i, pozycja2j];
+					}
+				}
 			}
 
 		}
