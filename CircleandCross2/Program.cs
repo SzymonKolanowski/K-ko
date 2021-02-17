771dcd4d2795dfb48c0ef98767bd6a78ec4d1e95
Szymon Kolanowski <szymonkolanowski@op.pl>
2021-02-17 08:46:21 +0100

    Na razie stanąłem że gra sie skonczy jeśli gracz co daje x da go w tej samej turze co gracz mający "o" w tej samej turze w to samo miejsce , jeśli da go w innej turze to nic się nie dzieje. Nie mam pomysłu gdzie tego if dać

--- CircleandCross2/Program.cs
+++ CircleandCross2/Program.cs
@@ -4,103 +4,228 @@ namespace CircleandCross2
 {
 	class Program
 	{
-		static string[,] Savearr(string[,] array)
-		{
-			for (int i = 1; i < array.Length; i++)
-			{
-				for (int j = 1; j < array.Length; j++)
-				{
-					array[i,j] = array[i,j] + array[i-1,j-1];// - wywala błąd " index was outside the bounds of the array" , próbowałem różnych opcji nie mam już pomysłow jak tą funkcję zapisać
-				}
-				
-			}
-			return array;
-		}
+
 		static void Main(string[] args)
 		{
-			Console.WriteLine("CircleandCross");
-			
-			string[,] array = new string[3,3] { {"[ ]","[ ]","[ ]" }, {"[ ]","[ ]","[ ]"}, {"[ ]","[ ]","[ ]" } };
-			string[,] Cumulativearr = Savearr(array);
-			for (int i = 0; i <array.GetLength(0); i++)
+			try
 			{
-				for (int j = 0; j < array.GetLength(1); j++)
+
+
+				Console.WriteLine("CircleandCross");
+
+				string[,] array = new string[3, 3] { { "[ ]", "[ ]", "[ ]" }, { "[ ]", "[ ]", "[ ]" }, { "[ ]", "[ ]", "[ ]" } };
+
+				for (int i = 0; i < array.GetLength(0); i++)
 				{
-					string arr = array[i,j];
-					Console.Write(arr);
+					for (int j = 0; j < array.GetLength(1); j++)
+					{
+						string arr = array[i, j];
+						Console.Write(arr);
+					}
+					Console.WriteLine();
 				}
-				Console.WriteLine();
-			}
 
-			Console.WriteLine("podaj pozycje 'o' w lini 'i'");
-			string tekst = Console.ReadLine();
-			int pozycja1i = int.Parse(tekst);
-			Console.WriteLine(" podaj pozycję 'o' w lini 'j' ");
-			string tekst2 = Console.ReadLine();
-			int pozycja1j = int.Parse(tekst2);
 
-			//string[] Cumulativearr = Savearr(array); - nie wiem jak zapisać funcję do tablicy 2-wymiarowej i w którym miejscu dać tą funkcję
-
-			for (int i = 0; i < array.GetLength(0); i++)
-			{
-				for (int j = 0; j < array.GetLength(1); j++)
+				for (int a = 1; a <= 5; a++)
 				{
-					
-					//if (i == pozycja1i)
+
+					Console.WriteLine("podaj pozycje 'o' w lini 'i'");
+					string tekst = Console.ReadLine();
+					int pozycja1i = int.Parse(tekst);
+					Console.WriteLine(" podaj pozycję 'o' w lini 'j' ");
+					string tekst2 = Console.ReadLine();
+					int pozycja1j = int.Parse(tekst2);
+
+					array[pozycja1i, pozycja1j] = "[o]";
+
+					for (int i = 0; i < array.GetLength(0); i++)
+					{
+						for (int j = 0; j < array.GetLength(1); j++)
+						{
+							string arr1 = array[i, j];
+							Console.Write(arr1);
+
+						}
+						Console.WriteLine();
+					}
+
+					if (array[0, 0] == "[o]" && array[0, 1] == "[o]" && array[0, 2] == "[o]")
+					{
+						Console.WriteLine("wygrał 'o' , konic gry");
+						break;
+					}
+
+					else if (array[1, 0] == "[o]" && array[1, 1] == "[o]" && array[1, 2] == "[o]")
+					{
+						Console.WriteLine("wygrał 'o' , konic gry");
+						break;
+					}
+
+					else if (array[2, 0] == "[o]" && array[2, 1] == "[o]" && array[2, 2] == "[o]")
+					{
+						Console.WriteLine("wygrał 'o' , konic gry");
+						break;
+					}
+
+					else if (array[0, 0] == "[o]" && array[1, 1] == "[o]" && array[2, 2] == "[o]")
+					{
+						Console.WriteLine("wygrał 'o' , konic gry");
+						break;
+					}
+
+					else if (array[0, 0] == "[o]" && array[1, 0] == "[o]" && array[2, 0] == "[o]")
+					{
+						Console.WriteLine("wygrał 'x' , konic gry");
+						break;
+					}
+
+					else if (array[0, 0] == "[o]" && array[1, 0] == "[o]" && array[2, 0] == "[o]")
+					{
+						Console.WriteLine("wygrał 'o' , konic gry");
+						break;
+					}
+
+					else if (array[0, 1] == "[o]" && array[1, 1] == "[o]" && array[2, 1] == "[o]")
+					{
+						Console.WriteLine("wygrał 'o' , konic gry");
+						break;
+					}
+
+					else if (array[0, 2] == "[o]" && array[1, 2] == "[o]" && array[2, 2] == "[o]")
+					{
+						Console.WriteLine("wygrał 'o' , konic gry");
+						break;
+					}
+
+					else if (array[0, 2] == "[o]" && array[1, 1] == "[o]" && array[2, 0] == "[o]")
+					{
+						Console.WriteLine("wygrał 'o' , konic gry");
+						break;
+					}
+
+
+
+					Console.WriteLine("podaj pozycję znaku 'x' w lini i");
+					string tekst3 = Console.ReadLine();
+					int pozycja2i = int.Parse(tekst3);
+					Console.WriteLine("podaj pozycję znaku 'x' w lini j");
+					string tekst4 = Console.ReadLine();
+					int pozycja2j = int.Parse(tekst4);
+
+					array[pozycja2i, pozycja2j] = "[x]";
+
+					for (int i = 0; i < array.GetLength(0); i++)
+					{
+						for (int j = 0; j < array.GetLength(1); j++)
+						{
+							string arr2 = array[i, j];
+							Console.Write(arr2);
+						}
+						Console.WriteLine();
+					}
+
+					//if (array[pozycja2i, pozycja2j] == array[pozycja1j, pozycja1j])
 					//{
-					//	Console.Write("o");
+					//	Console.WriteLine("podana pozycja jest już zajęta");
+					//	break;
 					//}
-					 if (j == pozycja1j )
+
+					if (array[0, 0] == "[x]" && array[0, 1] == "[x]" && array[0, 2] == "[x]")
 					{
-						Console.Write("o");
+						Console.WriteLine("wygrał 'x' , konic gry");
+						break;
 					}
-					else
+					else if (array[1, 0] == "[x]" && array[1, 1] == "[x]" && array[1, 2] == "[x]")
 					{
-						string arr1 = array[pozycja1i,pozycja1j ];
-						Console.Write(arr1);
+						Console.WriteLine("wygrał 'x' , konic gry");
+						break;
 					}
+
+					else if (array[2, 0] == "[x]" && array[2, 1] == "[x]" && array[2, 2] == "[x]")
+					{
+						Console.WriteLine("wygrał 'x' , konic gry");
+						break;
+					}
+
+					else if (array[0, 0] == "[x]" && array[1, 1] == "[x]" && array[2, 2] == "[x]")
+					{
+						Console.WriteLine("wygrał 'x' , konic gry");
+						break;
+					}
+
+					else if (array[0, 0] == "[x]" && array[1, 0] == "[x]" && array[2, 0] == "[x]")
+					{
+						Console.WriteLine("wygrał 'x' , konic gry");
+						break;
+					}
+
+					else if (array[0, 0] == "[x]" && array[1, 0] == "[x]" && array[2, 0] == "[x]")
+					{
+						Console.WriteLine("wygrał 'x' , konic gry");
+						break;
+					}
+
+					else if (array[0, 1] == "[x]" && array[1, 1] == "[x]" && array[2, 1] == "[x]")
+					{
+						Console.WriteLine("wygrał 'x' , konic gry");
+						break;
+					}
+
+					else if (array[0, 2] == "[x]" && array[1, 2] == "[x]" && array[2, 2] == "[x]")
+					{
+						Console.WriteLine("wygrał 'x' , konic gry");
+						break;
+					}
+
+					else if (array[0, 2] == "[x]" && array[1, 1] == "[x]" && array[2, 0] == "[x]")
+					{
+						Console.WriteLine("wygrał 'x' , konic gry");
+						break;
+					}
+
+					//if (array[0, 0] == array[pozycja1i, pozycja1j] && array[0, 0] == array[pozycja2i, pozycja2j])
+					//{
+					//	Console.WriteLine("pozycja jest już zajeta");
+					//	break;
+					//}
+
+					//if (array[pozycja1i,pozycja1j] == array[pozycja2i,pozycja2j])
+					//{
+					//	Console.WriteLine("pozycja była już zajęta");
+					//	break;
+					//}
+					if (pozycja2i == pozycja1i & pozycja2j == pozycja1j | pozycja1i == pozycja2i & pozycja1j == pozycja2j)
+					{
+						Console.WriteLine("podana współrzędna jest już zajęta ");
+						break;
+					}
+					//if (pozycja1i == pozycja2i && pozycja1j == pozycja2j)
+					//{
+					//	Console.WriteLine("podana współrzędna jest już zajeta przez 'x'");
+					//	break;
+					//}
+
+					//if (pozycja1i == pozycja1i && pozycja1j == pozycja1j || pozycja2i == pozycja2i && pozycja2j == pozycja2j)
+					//{
+					//	Console.WriteLine("już zająłeś tą współrzędną");
+					//	break;
+					//}
+
+
 				}
 
-				if (i == pozycja1i)
-				{
-					Console.Write("o");
-				}
-
-				//else
-				//{
-				//	string arr1 = array[pozycja1j];
-				//	Console.Write(arr1);
-				//}
-
-
-
-				Console.WriteLine();
-			
 				
+
 			}
 
-			Console.WriteLine("podaj pozycję znaku 'x' w lini i");
-			string tekst3 = Console.ReadLine();
-			int pozycja2i = int.Parse(tekst3);
-			Console.WriteLine("podaj pozycję znaku 'x' w lini j");
-			string tekst4 = Console.ReadLine();
-			int pozycja2j = int.Parse(tekst4);
-
-
-			for (int i = 0; i < array.GetLength(0); i++)
+			catch (Exception exception)
 			{
-				for (int j = 0; j < array.GetLength(1); j++)
-				{
-					if (j == pozycja2j)
-					{
-						Console.WriteLine("x");
-					}
-					else
-					{
-						string arr2 = array[pozycja2i, pozycja2j];
-					}
-				}
+
+				Console.WriteLine(exception.Message);
 			}
+
+			// - spisać wszystkie warunki wygranej , sprawdzić możliwe błędy, ulepszyć kod.
+
 
 		}
 	}
